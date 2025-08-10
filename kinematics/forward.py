69513afd77722525ea7178334b228d986ca9f1a8
Hakaisha <ixforce4@gmail.com>
2025-08-10 22:47:03 +0700

    little corrction

--- kinematics/forward.py
+++ kinematics/forward.py
@@ -2,11 +2,6 @@ import numpy as np
 import math
 
 def forward_kinematic(omega, r, L, W):
-    """
-    Forward kinematics: wheel speeds -> robot velocity
-    omega: [omega1, omega2, omega3, omega4] (rad/s)
-    Returns: [vx, vy, omega_z] (m/s, m/s, rad/s)
-    """
     M = (r/4) * np.array([
         [1, 1, 1, 1],                                    # vx
         [-1, 1, 1, -1],                                  # vy
